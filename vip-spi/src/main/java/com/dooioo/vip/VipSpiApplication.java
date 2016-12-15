dc8dccf967bb88875e46a884e72d89102ae26003
元赛 <vincent.van.yuan@gmail.com>
2016-12-15 15:47:39 +0800

    Initial commit

---
+++ vip-spi/src/main/java/com/dooioo/vip/VipSpiApplication.java
@@ -0,0 +1,12 @@
+package com.dooioo.vip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipSpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipSpiApplication.class, args);
+	}
+}
