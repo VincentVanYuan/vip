7433a0fbc3903f0cc7e5ffa731a902efaa3204fc
元赛 <vincent.van.yuan@gmail.com>
2016-12-19 16:20:22 +0800

    测试测试测试

--- vip-server/src/main/java/com/dooioo/vip/VipServerApplication.java
+++ vip-server/src/main/java/com/dooioo/vip/VipServerApplication.java
@@ -1,12 +1,26 @@
 package com.dooioo.vip;
 
+import com.dooioo.se.lorik.core.annotation.EnableBuiltinRestSupport;
+import com.dooioo.vip.spi.UserSpi;
+import com.lianjia.sh.xcount.point.EnableXcount;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableXcount
+@EnableEurekaClient
+@Configuration
+@EnableFeignClients(basePackageClasses = {UserSpi.class})
+@EnableBuiltinRestSupport
+@EnableScheduling
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 public class VipServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VipServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VipServerApplication.class, args);
+    }
 }
